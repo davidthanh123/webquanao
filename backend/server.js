625f9ca2b982a765cae40e0b31181d628c5cf074
davidthanh123 <thanhzata123@gmail.com>
2026-04-01 14:30:21 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'https://webquanao.vercel.app',
+    'https://webquanao-seven.vercel.app',
     'http://localhost:3000'
   ],
   credentials: true
