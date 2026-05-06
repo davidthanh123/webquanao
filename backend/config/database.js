9bd0a9b098b618519c711a14981b6898fe96f8f3
davidthanh123 <thanhzata123@gmail.com>
2026-05-06 22:13:18 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,19 +2,20 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE || 'railway',
-  process.env.MYSQL_USER || 'root',
-  process.env.MYSQL_PASSWORD || 'YtHgUOpEgPnMLRYAjUUUQAzCPOAWLfBY',
+  process.env.MYSQL_DATABASE || 'defaultdb',
+  process.env.MYSQL_USER || 'avnadmin',
+  process.env.MYSQL_PASSWORD,
   {
-    // Nếu có biến môi trường MYSQL_HOST (do bạn đặt trong .env hoặc Railway tự có) thì dùng, 
-    // nếu không thì mặc định dùng cái host Public để bạn Seed tại máy.
-    host: process.env.MYSQL_HOST || 'interchange.proxy.rlwy.net',
-    
-    // Tương tự cho Port
-    port: process.env.MYSQL_PORT || 27854,
-    
+    host: process.env.MYSQL_HOST || 'mysql-1577d162-thanhzata123-2174.h.aivencloud.com',
+    port: process.env.MYSQL_PORT || 28424,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Aiven SSL
+      }
+    }
   }
 );
 
