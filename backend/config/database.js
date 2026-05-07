fc96423820918657c6576b1cf87ced3c38897efa
davidthanh123 <thanhzata123@gmail.com>
2026-05-07 17:20:11 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize(
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // Aiven SSL
+        rejectUnauthorized: false,
       }
     }
   }
